64e3f363994f5106974a5146b93b987af0a8363c
juanlluchrojo <juanlluchrojo@uoc.edu>
2024-08-13 02:50:52 +0200

    holamundo.c

---
+++ holamundo.c
@@ -0,0 +1,21 @@
+/*Nombre: Hola Mundo
+Fecha: 13/08/2024
+Autor: Juan Lluch Rojo
+Motivo: Creación del primer fichero en C
+*/
+
+//Declaración de librerías
+#include <stdio.h>
+
+//Función Main
+int main(){
+
+    /*Esta función 
+    imprime la palabra 
+    Hola Mundo en la terminal
+    */
+    printf("Hola Mundo\n");
+    
+    //Termina la ejecución del programa
+    return 0;
+}
